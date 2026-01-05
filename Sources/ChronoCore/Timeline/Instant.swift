c72c33fc161e28c19a54e77d540c61f89d9b4aeb
Dimas Prabowo <58059518+98prabowo@users.noreply.github.com>
2026-01-05 20:41:59 +0700

    feat: create absolute timeline instant structure

---
+++ Sources/ChronoCore/Timeline/Instant.swift
@@ -0,0 +1,138 @@
+import ChronoMath
+
+public struct Instant: Equatable, Hashable, Sendable {
+    public let seconds: Int64
+    public let nanoseconds: Int32
+
+    @inlinable
+    public init(seconds: Int64, nanoseconds: Int32 = 0) {
+        precondition(
+            nanoseconds >= 0 && nanoseconds < NanoSeconds.perSecond32,
+            "nanoseconds exceeds supported Instant's fraction range."
+        )
+
+        self.seconds = seconds
+        self.nanoseconds = nanoseconds
+    }
+}
+
+// MARK: - Constructors
+
+public extension Instant {
+    static let zero: Self = .init(seconds: 0, nanoseconds: 0)
+}
+
+// MARK: - Comparability
+
+extension Instant: Comparable {
+    @inlinable
+    public static func < (lhs: Self, rhs: Self) -> Bool {
+        if lhs.seconds == rhs.seconds {
+            return lhs.nanoseconds < rhs.nanoseconds
+        }
+        return lhs.seconds < rhs.seconds
+    }
+}
+
+// MARK: - Core Accessor
+
+public extension Instant {
+    @inlinable
+    var timestamp: Int64 {
+        seconds
+    }
+
+    @inlinable
+    var timestampMicroseconds: Int64 {
+        seconds * MicroSeconds.perSecond64 + Int64(nanoseconds) / NanoSeconds.perMicroSecond64
+    }
+
+    @inlinable
+    var timestampNanoseconds: Int64 {
+        seconds * NanoSeconds.perSecond64 + Int64(nanoseconds)
+    }
+
+    @inlinable
+    var timestampNanosecondsChecked: Int64? {
+        let (secPart, overflowMul) = seconds.multipliedReportingOverflow(by: NanoSeconds.perSecond64)
+        if overflowMul { return nil }
+
+        let (total, overflowSum) = secPart.addingReportingOverflow(Int64(nanoseconds))
+        if overflowSum { return nil }
+
+        return total
+    }
+}
+
+// MARK: - Arithmetic
+
+public extension Instant {
+    @inlinable
+    func advanced(bySeconds seconds: Int64, nanoseconds: Int64 = 0) -> Self {
+        let totalNanos = Int64(self.nanoseconds) + nanoseconds
+        let extraSeconds = floorDiv(totalNanos, NanoSeconds.perSecond64)
+        let remNanos = floorMod(totalNanos, NanoSeconds.perSecond64)
+        return Self(
+            seconds: self.seconds + seconds + extraSeconds,
+            nanoseconds: Int32(remNanos)
+        )
+    }
+
+    @inlinable
+    func advanced(by duration: Duration) -> Self {
+        advanced(
+            bySeconds: duration.seconds,
+            nanoseconds: Int64(duration.nanoseconds)
+        )
+    }
+}
+
+// MARK: - Addition
+
+public extension Instant {
+    @inlinable
+    static func + (lhs: Self, rhs: Duration) -> Self {
+        lhs.advanced(by: rhs)
+    }
+
+    @inlinable
+    static func + (lhs: Duration, rhs: Self) -> Self {
+        rhs.advanced(by: lhs)
+    }
+
+    @inlinable
+    static func += (lhs: inout Self, rhs: Duration) {
+        lhs = lhs + rhs
+    }
+}
+
+// MARK: - Substraction
+
+public extension Instant {
+    @inlinable
+    static func - (lhs: Self, rhs: Self) -> Duration {
+        let secDiff = lhs.seconds - rhs.seconds
+        let nanoDiff = Int64(lhs.nanoseconds) - Int64(rhs.nanoseconds)
+
+        let extraSec = floorDiv(nanoDiff, NanoSeconds.perSecond64)
+        let normalizedNanos = floorMod(nanoDiff, NanoSeconds.perSecond64)
+
+        return Duration(
+            seconds: secDiff + extraSec,
+            nanoseconds: normalizedNanos
+        )
+    }
+
+    @inlinable
+    static func - (lhs: Self, rhs: Duration) -> Self {
+        lhs.advanced(
+            bySeconds: -rhs.seconds,
+            nanoseconds: -Int64(rhs.nanoseconds)
+        )
+    }
+
+    @inlinable
+    static func -= (lhs: inout Self, rhs: Duration) {
+        lhs = lhs - rhs
+    }
+}
