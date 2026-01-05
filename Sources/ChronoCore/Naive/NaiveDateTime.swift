785ea77fb6876485f9feaa2e0e57a9f2652ee3df
Dimas Prabowo <58059518+98prabowo@users.noreply.github.com>
2026-01-05 20:32:14 +0700

    feat: create naive date time

---
+++ Sources/ChronoCore/Naive/NaiveDateTime.swift
@@ -0,0 +1,165 @@
+import ChronoMath
+
+public struct NaiveDateTime: Equatable, Hashable, Sendable {
+    public let date: NaiveDate
+    public let time: NaiveTime
+
+    @inlinable
+    public init(date: NaiveDate, time: NaiveTime) {
+        self.date = date
+        self.time = time
+    }
+
+    @inlinable
+    public init?(
+        year: Int32,
+        month: Int,
+        day: Int,
+        hour: Int = 0,
+        minute: Int = 0,
+        second: Int = 0,
+        nanosecond: Int = 0
+    ) {
+        guard let date = NaiveDate(year: year, month: month, day: day),
+              let time = NaiveTime(hour: hour, minute: minute, second: second, nanosecond: nanosecond)
+        else { return nil }
+
+        self.date = date
+        self.time = time
+    }
+}
+
+// MARK: - Comparability
+
+extension NaiveDateTime: Comparable {
+    @inlinable
+    public static func < (lhs: Self, rhs: Self) -> Bool {
+        if lhs.date == rhs.date {
+            return lhs.time < rhs.time
+        }
+
+        return lhs.date < rhs.date
+    }
+}
+
+// MARK: - Constructors
+
+public extension NaiveDateTime {
+    static let min: Self = .init(date: .min, time: .min)
+    static let max: Self = .init(date: .max, time: .max)
+}
+
+// MARK: - Arithmetic
+
+public extension NaiveDateTime {
+    @inlinable
+    func advanced(bySeconds seconds: Int64, nanoseconds: Int64 = 0) -> Self {
+        let totalNanos = time.nanosecondsSinceMidnight + nanoseconds
+
+        let extraDaysFromNanos = floorDiv(totalNanos, NanoSeconds.perDay64)
+        let remNanos = floorMod(totalNanos, NanoSeconds.perDay64)
+
+        let extraDaysFromSecs = floorDiv(seconds, Seconds.perDay64)
+        let remSecs = floorMod(seconds, Seconds.perDay64)
+
+        let finalNanosTotal = remNanos + (remSecs * NanoSeconds.perSecond64)
+        let finalDayDelta = floorDiv(finalNanosTotal, NanoSeconds.perDay64)
+        let finalNanos = floorMod(finalNanosTotal, NanoSeconds.perDay64)
+
+        return Self(
+            date: date.advanced(byDays: extraDaysFromNanos + extraDaysFromSecs + finalDayDelta),
+            time: NaiveTime(nanosecondsSinceMidnight: finalNanos)
+        )
+    }
+
+    @inlinable
+    func advanced(by duration: Duration) -> Self {
+        advanced(
+            bySeconds: duration.seconds,
+            nanoseconds: Int64(duration.nanoseconds)
+        )
+    }
+}
+
+// MARK: - Addition
+
+public extension NaiveDateTime {
+    @inlinable
+    static func + (lhs: Self, rhs: Duration) -> Self {
+        lhs.advanced(by: rhs)
+    }
+
+    @inlinable
+    static func + (lhs: Duration, rhs: Self) -> Self {
+        rhs.advanced(by: lhs)
+    }
+
+    @inlinable
+    static func + (lhs: Self, rhs: CalendarInterval) -> Self {
+        let newDate = lhs.date + rhs
+
+        let currentNanos = lhs.time.nanosecondsSinceMidnight
+        let totalNanos = currentNanos + rhs.nanosecond
+
+        let dayAdjustment = floorDiv(totalNanos, NanoSeconds.perDay64)
+        let finalNanos = floorMod(totalNanos, NanoSeconds.perDay64)
+
+        let finalDate = NaiveDate(daysSinceEpoch: newDate.daysSinceEpoch + dayAdjustment)
+        let finalTime = NaiveTime(nanosecondsSinceMidnight: finalNanos)
+
+        return Self(date: finalDate, time: finalTime)
+    }
+
+    @inlinable
+    static func += (lhs: inout Self, rhs: Duration) {
+        lhs = lhs + rhs
+    }
+
+    @inlinable
+    static func += (lhs: inout Self, rhs: CalendarInterval) {
+        lhs = lhs + rhs
+    }
+}
+
+// MARK: - Substraction
+
+public extension NaiveDateTime {
+    @inlinable
+    static func - (lhs: Self, rhs: Self) -> Duration {
+        let dayDiff = lhs.date.daysSinceEpoch - rhs.date.daysSinceEpoch
+        let nanoDiff = lhs.time.nanosecondsSinceMidnight - rhs.time.nanosecondsSinceMidnight
+
+        let totalSec = dayDiff * Seconds.perDay64
+
+        let extraSec = floorDiv(nanoDiff, NanoSeconds.perSecond64)
+        let normalizedNanos = floorMod(nanoDiff, NanoSeconds.perSecond64)
+
+        return Duration(
+            seconds: totalSec + extraSec,
+            nanoseconds: normalizedNanos
+        )
+    }
+
+    @inlinable
+    static func - (lhs: Self, rhs: Duration) -> Self {
+        lhs.advanced(
+            bySeconds: -rhs.seconds,
+            nanoseconds: -Int64(rhs.nanoseconds)
+        )
+    }
+
+    @inlinable
+    static func - (lhs: Self, rhs: CalendarInterval) -> Self {
+        return lhs + -rhs
+    }
+
+    @inlinable
+    static func -= (lhs: inout Self, rhs: Duration) {
+        lhs = lhs - rhs
+    }
+
+    @inlinable
+    static func -= (lhs: inout Self, rhs: CalendarInterval) {
+        lhs = lhs - rhs
+    }
+}
