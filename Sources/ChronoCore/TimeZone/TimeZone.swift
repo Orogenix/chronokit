5874c544e27b05199731f72fb911d83dfd869f00
Dimas Prabowo <58059518+98prabowo@users.noreply.github.com>
2026-01-05 20:45:21 +0700

    feat: support DST on time zone protocol

--- Sources/ChronoCore/TimeZone/TimeZone.swift
+++ Sources/ChronoCore/TimeZone/TimeZone.swift
@@ -4,4 +4,65 @@ public protocol TimeZoneProtocol: Equatable, Hashable, Sendable {
 
     /// Returns the offset in Duration from UTC for a specific UTC timestamp.
     func offset(for instant: Instant) -> Duration
+
+    /// Returns the offset in Duration from local date time.
+    func offset(for local: NaiveDateTime) -> LocalOffset
+}
+
+public enum LocalOffset: Equatable, Hashable, Sendable {
+    case unique(Duration)
+    case ambiguous(earlier: Duration, later: Duration)
+    case invalid
+}
+
+public extension LocalOffset {
+    @inline(__always)
+    func resolve(using policy: DSTResolutionPolicy) -> Duration? {
+        switch self {
+        case let .unique(offset):
+            offset
+
+        case let .ambiguous(earlier, later):
+            switch policy {
+            case .preferEarlier:
+                earlier
+            case .preferLater:
+                later
+            case .strict:
+                nil
+            }
+
+        case .invalid:
+            nil
+        }
+    }
+}
+
+public enum DSTResolutionPolicy: Equatable, Hashable, Sendable {
+    case preferEarlier
+    case preferLater
+    case strict
+}
+
+public enum TimeZoneSign: Character, Equatable, Hashable, Sendable {
+    case minus = "-"
+    case plus = "+"
+
+    @inlinable
+    public init?(symbol: String) {
+        guard let char = symbol.first, symbol.count == 1 else { return nil }
+        self.init(rawValue: char)
+    }
+}
+
+public extension TimeZoneSign {
+    @inlinable
+    var multiplier: Int {
+        self == .plus ? 1 : -1
+    }
+
+    @inlinable
+    func apply<T: BinaryInteger>(to value: T) -> T {
+        value * T(multiplier)
+    }
 }
