545253e557ddbbeae184d46bb0f3979600ac9bbb
Dimas Prabowo <58059518+98prabowo@users.noreply.github.com>
2026-01-05 20:02:43 +0700

    feat: add ascii helper

---
+++ Sources/ChronoMath/Ascii.swift
@@ -0,0 +1,18 @@
+@usableFromInline
+package enum ASCII {
+    @usableFromInline package static let space: UInt8 = 32 // ' '
+    @usableFromInline package static let plus: UInt8 = 43 // '+'
+    @usableFromInline package static let comma: UInt8 = 44 // ','
+    @usableFromInline package static let dash: UInt8 = 45 // '-'
+    @usableFromInline package static let dot: UInt8 = 46 // '.'
+    @usableFromInline package static let zero: UInt8 = 48 // '0'
+    @usableFromInline package static let colon: UInt8 = 58 // ':'
+    @usableFromInline package static let charD: UInt8 = 68 // 'D'
+    @usableFromInline package static let charH: UInt8 = 72 // 'H'
+    @usableFromInline package static let charM: UInt8 = 77 // 'M'
+    @usableFromInline package static let charP: UInt8 = 80 // 'P'
+    @usableFromInline package static let charT: UInt8 = 84 // 'T'
+    @usableFromInline package static let charY: UInt8 = 89 // 'Y'
+    @usableFromInline package static let charS: UInt8 = 83 // 'S'
+    @usableFromInline package static let charZ: UInt8 = 90 // 'Z'
+}
