5fc2518639c48ba2a31752f7a14d64c76c2360cf
Dimas Prabowo <58059518+98prabowo@users.noreply.github.com>
2026-04-17 01:07:43 +0700

    feat(ascii): add ascii 0 to 31

--- Sources/ChronoMath/Ascii.swift
+++ Sources/ChronoMath/Ascii.swift
@@ -1,5 +1,39 @@
 @usableFromInline
 package enum ASCII {
+    // --- Control Characters ---
+    @usableFromInline package static let nul: UInt8 = 0 // Null
+    @usableFromInline package static let soh: UInt8 = 1 // Start of Heading
+    @usableFromInline package static let stx: UInt8 = 2 // Start of Text
+    @usableFromInline package static let etx: UInt8 = 3 // End of Text
+    @usableFromInline package static let eot: UInt8 = 4 // End of Transmission
+    @usableFromInline package static let enq: UInt8 = 5 // Enquiry
+    @usableFromInline package static let ack: UInt8 = 6 // Acknowledge
+    @usableFromInline package static let bel: UInt8 = 7 // Bell
+    @usableFromInline package static let bs: UInt8 = 8 // Backspace
+    @usableFromInline package static let tab: UInt8 = 9 // Horizontal Tab
+    @usableFromInline package static let lf: UInt8 = 10 // Line Feed
+    @usableFromInline package static let vt: UInt8 = 11 // Vertical Tab
+    @usableFromInline package static let ff: UInt8 = 12 // Form Feed
+    @usableFromInline package static let cr: UInt8 = 13 // Carriage Return
+    @usableFromInline package static let so: UInt8 = 14 // Shift Out
+    @usableFromInline package static let si: UInt8 = 15 // Shift In
+    @usableFromInline package static let dle: UInt8 = 16 // Data Link Escape
+    @usableFromInline package static let dc1: UInt8 = 17 // Device Control 1
+    @usableFromInline package static let dc2: UInt8 = 18 // Device Control 2
+    @usableFromInline package static let dc3: UInt8 = 19 // Device Control 3
+    @usableFromInline package static let dc4: UInt8 = 20 // Device Control 4
+    @usableFromInline package static let nak: UInt8 = 21 // Negative Acknowledge
+    @usableFromInline package static let syn: UInt8 = 22 // Synchronous Idle
+    @usableFromInline package static let etb: UInt8 = 23 // End of Trans. Block
+    @usableFromInline package static let can: UInt8 = 24 // Cancel
+    @usableFromInline package static let em: UInt8 = 25 // End of Medium
+    @usableFromInline package static let sub: UInt8 = 26 // Substitute
+    @usableFromInline package static let esc: UInt8 = 27 // Escape
+    @usableFromInline package static let fs: UInt8 = 28 // File Separator
+    @usableFromInline package static let gs: UInt8 = 29 // Group Separator
+    @usableFromInline package static let rs: UInt8 = 30 // Record Separator
+    @usableFromInline package static let us: UInt8 = 31 // Unit Separator
+
     // --- Printable Punctuations & Symbols ---
     @usableFromInline package static let space: UInt8 = 32 // ' '
     @usableFromInline package static let bang: UInt8 = 33 // '!'
