8ba483618af96382e6d1b21ccae6c9addbaadc19
Dimas Prabowo <58059518+98prabowo@users.noreply.github.com>
2026-04-26 22:03:12 +0700

    fix(system): update candidates resource path

--- Sources/ChronoSystem/FileSystem/ResourceLocator.swift
+++ Sources/ChronoSystem/FileSystem/ResourceLocator.swift
@@ -17,7 +17,8 @@ package enum ResourceLocator {
 
         let path = String(cString: info.dli_fname)
         let baseDir = String(path.split(separator: "/").dropLast().joined(separator: "/"))
-        let candidate = "/\(baseDir)/Resources/\(name)"
+        let bundleName = "ChronoTZ_ChronoTZ.bundle"
+        let candidate = "/\(baseDir)/\(bundleName)/\(name)"
 
         print("✅ Found \(name) at: \(candidate)")
         return access(candidate, F_OK) == 0 ? candidate : nil
